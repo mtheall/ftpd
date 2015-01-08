c5a42ca9b109c59d8d4531f767dadf30d6fd98ea
Dave Murphy <davem@devkitpro.org>
2015-01-08 14:01:09 +0000

    add header for memcpy

--- source/gfx.c
+++ source/gfx.c
@@ -2,6 +2,8 @@
 #include <3ds.h>
 #endif
 
+#include <string.h>
+
 /* Function to draw sprite, from smea/3ds_hb_menu */
 void gfxDrawSprite(gfxScreen_t screen, gfx3dSide_t side, u8* spriteData, u16 width, u16 height, s16 x, s16 y)
 {
