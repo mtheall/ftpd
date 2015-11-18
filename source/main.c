fdcb12f2999246f2b96fe514e04270f652dbcc06
WoniMK7 <wonimk7@gmail.com>
2015-11-18 21:15:13 +0100

    Update main.c

--- source/main.c
+++ source/main.c
@@ -61,11 +61,12 @@ int
 main(int  argc,
      char *argv[])
 {
-aptOpenSession();
-APT_SetAppCpuTimeLimit(NULL,0);
-aptCloseSession();
+
 #ifdef _3DS
   /* initialize needed 3DS services */
+  aptOpenSession();
+  APT_SetAppCpuTimeLimit(NULL,0);
+  aptCloseSession();
   gfxInitDefault();
   gfxSet3D(false);
 #endif
